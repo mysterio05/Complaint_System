2adf98555a35dcb9b493bce956eda98c74060c39
janvi-rajesh <janvi171005@gmail.com>
2026-06-11 16:29:20 +0530

    completed admin complaint list and status update pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from './pages/Dashboard'
+import AdminComplaintList from './pages/AdminComplaintList'
+import StatusUpdate from './pages/StatusUpdate'
 
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<Dashboard/>}/>
+      <Route path='/admin' element={<AdminComplaintList/>}/>
+      <Route path='/admin/update/:id' element={<StatusUpdate/>}/>
     </Routes>
     </>
   )
