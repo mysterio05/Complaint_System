9bbe69f4b45ad9a883e7f8174a90012403535573
MYSTERIO <gowthamappu55231@gmail.com>
2026-06-12 17:15:27 +0530

    Dashboard completed second

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -3,11 +3,22 @@ import Dashboardbanner from '../components/Dashboardbanner'
 import DashboardCard from '../components/DashboardCard'
 import ComplaintChart from '../components/ComplaintChart'
 const AdminDashboard = () => {
+    const data = [
+      { category: "Classroom", complaints: 10 },
+      { category: "Laboratory", complaints: 8 },
+      { category: "Hostel", complaints: 6 },
+      { category: "Library", complaints: 4 },
+      { category: "Wi-Fi", complaints: 12 },
+      { category: "Electrical", complaints: 5 },
+      { category: "Water", complaints: 3 },
+      { category: "Clean", complaints: 7 },
+      { category: "Other", complaints: 2 }
+    ];
   return (
     <div>
       <Dashboardbanner />
       <DashboardCard />
-      <ComplaintChart />
+       <ComplaintChart complaint={data}/>
       </div>
   )
 }
